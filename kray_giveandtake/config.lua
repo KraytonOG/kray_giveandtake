04819476b3e8d6d55c55350a63a1e05b06407f60
KraytonOG <116412443+KraytonOG@users.noreply.github.com>
2025-09-15 14:50:45 -0400

    Add files via upload

---
+++ kray_giveandtake/config.lua
@@ -0,0 +1,31 @@
+-- config.lua
+Config = {}
+
+-- Define a table of items that can be used.
+-- Format: ['item_name_to_use'] = { removeItem = 'item_to_remove', giveItem = 'item_to_give' }
+-- This setup assumes the item to be removed is the same as the item being used.
+Config.UsableItems = {
+    -- Example 1: Use a 'first_aid_kit' and get a 'bandage'
+    ['first_aid_kit'] = {
+        itemToRemove = 'first_aid_kit',
+        countToRemove = 1,
+        itemToGive = 'bandage',
+        countToGive = 2,
+    },
+    
+    -- Example 2: Use a 'water_bottle' and get an 'empty_bottle'
+    ['water_bottle'] = {
+        itemToRemove = 'water_bottle',
+        countToRemove = 1,
+        itemToGive = 'empty_bottle',
+        countToGive = 1,
+    },
+
+    -- Example 3: Use 'raw_ore' and get 'refined_metal'
+    ['raw_ore'] = {
+        itemToRemove = 'raw_ore',
+        countToRemove = 5, -- You can require multiple items to be used
+        itemToGive = 'refined_metal',
+        countToGive = 1,
+    },
+}
---
+++ kray_giveandtake/fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Script Developed by Krayton Oghee
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Krayton Oghee'
+description 'A resource for using an item and giving an item back'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'qbx_core',
+    'ox_inventory'
+}
